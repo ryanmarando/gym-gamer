bf57723c70d5d03eac30ad5d1faa62e03b5b0ed5
Ryan Marando <marandoryan@gmail.com>
2025-08-07 08:15:48 -0400

    added admin portal react app

---
+++ admin/src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div
+            style={{
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                fontSize: "2rem",
+                fontFamily: "sans-serif",
+            }}
+        >
+            Coming soon!
+        </div>
+    );
+}
+
+export default App;
